0f0f3cb7ac1616eb5abbd0191a20f2a60f99284f
Steffen Grøndahl <mail@steffengrondahl.dk>
2016-11-09 21:40:22 +0100

    Remove project implemented.

--- selfstudy-rest/src/main/java/nu/steffengrondahl/selfstudy/rest/ProjectLightResource.java
+++
@@ -1,7 +0,0 @@
-package nu.steffengrondahl.selfstudy.rest;
-
-/**
- * Created by Steffen on 30-10-2016.
- */
-public class ProjectLightResource {
-}
--- selfstudy-rest/src/main/java/nu/steffengrondahl/selfstudy/rest/ProjectResource.java
+++ selfstudy-rest/src/main/java/nu/steffengrondahl/selfstudy/rest/ProjectResource.java
@@ -6,18 +6,19 @@ import nu.steffengrondahl.selfstudy.persist.domain.EstimateEntity;
 import nu.steffengrondahl.selfstudy.persist.domain.PriorityEntity;
 import nu.steffengrondahl.selfstudy.persist.domain.ProjectEntity;
 import nu.steffengrondahl.selfstudy.persist.domain.StatusEntity;
-import nu.steffengrondahl.selfstudy.rest.model.DAOFactory;
 import nu.steffengrondahl.selfstudy.rest.model.EstimateDTO;
-import nu.steffengrondahl.selfstudy.rest.model.GenericDAO;
 import nu.steffengrondahl.selfstudy.rest.model.PriorityDTO;
 import nu.steffengrondahl.selfstudy.rest.model.ProjectDTO;
 import nu.steffengrondahl.selfstudy.rest.model.ProjectLightDTO;
 import nu.steffengrondahl.selfstudy.rest.model.StatusDTO;
 
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -93,17 +94,15 @@ public class ProjectResource {
         projectDTO.setGoals(projectEntity.getGoals());
         projectDTO.setActions(projectEntity.getActions());
         LocalDate start = projectEntity.getStart();
-        if(start != null) {
+        if (start != null) {
             projectDTO.setStart(DateTimeFormatter.ISO_LOCAL_DATE.format(start));
-        }
-        else {
+        } else {
             projectDTO.setStart("");
         }
         LocalDate deadline = projectEntity.getDeadline();
-        if(deadline != null) {
+        if (deadline != null) {
             projectDTO.setDeadline(DateTimeFormatter.ISO_LOCAL_DATE.format(deadline));
-        }
-        else {
+        } else {
             projectDTO.setDeadline("");
         }
 
@@ -122,7 +121,7 @@ public class ProjectResource {
         statusDTO.setName(projectEntity.getStatus().getName());
         projectDTO.setStatus(statusDTO);
 
-        for(ProjectEntity pe : projectEntity.getPresupposed()) {
+        for (ProjectEntity pe : projectEntity.getPresupposed()) {
             ProjectLightDTO projectLightDTO = new ProjectLightDTO();
             projectLightDTO.setId(pe.getId());
             projectLightDTO.setDescription(pe.getDescription());
@@ -140,7 +139,7 @@ public class ProjectResource {
             projectDTO.getPresupposed().add(projectLightDTO);
         }
 
-        for(ProjectEntity pe : projectEntity.getSubsequent()) {
+        for (ProjectEntity pe : projectEntity.getSubsequent()) {
             ProjectLightDTO projectLightDTO = new ProjectLightDTO();
             projectLightDTO.setId(pe.getId());
             projectLightDTO.setDescription(pe.getDescription());
@@ -158,7 +157,7 @@ public class ProjectResource {
             projectDTO.getSubsequent().add(projectLightDTO);
         }
 
-        for(ProjectEntity pe : projectEntity.getLinkable()) {
+        for (ProjectEntity pe : projectEntity.getLinkable()) {
             ProjectLightDTO projectLightDTO = new ProjectLightDTO();
             projectLightDTO.setId(pe.getId());
             projectLightDTO.setDescription(pe.getDescription());
@@ -190,15 +189,13 @@ public class ProjectResource {
         LocalDate start = null;
         try {
             start = LocalDate.parse(projectDTO.getStart(), DateTimeFormatter.ISO_LOCAL_DATE);
-        }
-        catch(DateTimeParseException e) {
+        } catch (DateTimeParseException e) {
         }
         projectEntity.setStart(start);
         LocalDate deadline = null;
         try {
             deadline = LocalDate.parse(projectDTO.getDeadline(), DateTimeFormatter.ISO_LOCAL_DATE);
-        }
-        catch(DateTimeParseException e) {
+        } catch (DateTimeParseException e) {
         }
         projectEntity.setDeadline(deadline);
 
@@ -221,4 +218,61 @@ public class ProjectResource {
         return Response.created(uri).build();
     }
 
+    @DELETE
+    @Path("{id: \\d+}")
+    public void delete(@PathParam("id") int id) {
+        ProjectEntity projectEntity = new ProjectEntity();
+        projectEntity.setId(id);
+        ProjectEntityDAO dao = new ProjectEntityDAO();
+        dao.delete(projectEntity);
+        // No need to return a Response object, as returning nothing (void)
+        // implies returning HTTP 204 No Content
+    }
+
+    @PUT
+    @Path("{id: \\d+}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void update(@PathParam("id") int id, ProjectDTO projectDTO) {
+        if (projectDTO.getId() != id) {
+            throw new BadRequestException("Requested id does not match id in json data");
+        }
+        ProjectEntity projectEntity = new ProjectEntity();
+        projectEntity.setId(id);
+        projectEntity.setDescription(projectDTO.getDescription());
+        projectEntity.setGoals(projectDTO.getGoals());
+        projectEntity.setActions(projectDTO.getActions());
+
+        LocalDate start = null;
+        try {
+            start = LocalDate.parse(projectDTO.getStart(), DateTimeFormatter.ISO_LOCAL_DATE);
+        } catch (DateTimeParseException e) {
+        }
+        projectEntity.setStart(start);
+        LocalDate deadline = null;
+        try {
+            deadline = LocalDate.parse(projectDTO.getDeadline(), DateTimeFormatter.ISO_LOCAL_DATE);
+        } catch (DateTimeParseException e) {
+        }
+        projectEntity.setDeadline(deadline);
+
+        EstimateEntity estimateEntity = new EstimateEntity();
+        estimateEntity.setId(projectDTO.getEstimate().getId());
+        projectEntity.setEstimate(estimateEntity);
+
+        PriorityEntity priorityEntity = new PriorityEntity();
+        priorityEntity.setId(projectDTO.getPriority().getId());
+        projectEntity.setPriority(priorityEntity);
+
+        StatusEntity statusEntity = new StatusEntity();
+        statusEntity.setId(projectDTO.getStatus().getId());
+        projectEntity.setStatus(statusEntity);
+
+        // TODO: Fetch presupposed and subsequent
+
+
+        ProjectEntityDAO dao = new ProjectEntityDAO();
+        dao.update(projectEntity);
+        // No need to return a Response object, as returning nothing (void)
+        // implies returning HTTP 204 No Content
+    }
 }
